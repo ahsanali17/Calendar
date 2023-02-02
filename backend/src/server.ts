4efac965ffc6f10ee1a47d7581406c7701f6fb7a
ahsanali17 <ahsantime1@gmail.com>
2023-02-01 22:54:51 -0500

    initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,87 @@
+import express, {Application, Request, Response, NextFunction}  from 'express';
+import session from 'express-session';
+import http from 'http';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import MongoStore from 'connect-mongo';
+import passport from 'passport';
+
+import Logging from './Library/Logging';
+import {config} from './config/index';
+// import {routes} from './routes/index';
+
+const Server: Application = express();
+
+const port: number = config.server.port;
+const CLIENT_URL: string = `http://localhost:${port}`;
+
+
+/** Connect to MongoDB */
+mongoose
+.connect(`${config.mongo.url}/calendar_web_app`, {retryWrites: true, w: 'majority'})
+.then(() => {
+  Logging.info('Connected to MongoDB.');
+  StartServer();
+})
+.catch((error) => {
+  Logging.error('Unable to connect: ');
+  Logging.error(error);
+});
+
+/** Server Starter Function  */
+const StartServer = () => {
+  // Log Relevant Info on Server Startup [URL, IP Address, Server Status]
+  Server.use((req: Request, res: Response, next: NextFunction) => {
+		// Log The Request
+		Logging.info(`Incoming => Method [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+		res.on('finish', () => {
+			// Log The Response
+			Logging.info(`Incoming => Method [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`);
+		});
+
+		next();
+	});
+
+	Server.use(express.urlencoded({ extended: true }));
+	Server.use(express.json());
+
+  // Cors Check
+  Server.use(cors({ credentials: true, origin: CLIENT_URL }));
+
+	// Rules of our API
+	Server.use((req: Request, res: Response, next: NextFunction) => {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+		if (req.method == 'OPTIONS') {
+			res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+			return res.status(200).json({});
+		}
+
+		next();
+	});
+
+  /** API ROUTES */
+    // Routes
+    // Server.use('/users', userRoutes);
+
+
+    // Server API Check
+    Server.get('/ping', (req: Request, res: Response) => {
+      res.status(200).json({message: 'pong'});
+    });
+
+  // Error Handling
+	Server.use((req: Request, res: Response, next: NextFunction) => {
+		const error = new Error('Not Found');
+		Logging.error(error);
+
+		return res.status(404).json({ message: error.message });
+	});
+
+  // Log Server Port
+	http.createServer(Server).listen(port, () => Logging.info(`Server is running on port ${port}.`));
+}
+
+
