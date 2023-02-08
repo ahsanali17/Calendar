ce8767168a46744c2b3c00cf1fc8ac4f4f3cc1bc
ahsanali17 <ahsantime1@gmail.com>
2023-02-08 17:24:09 -0500

    components added calendar is coming along

--- frontend/src/components/Calendar/Calendar.tsx
+++ frontend/src/components/Calendar/Calendar.tsx
@@ -18,17 +18,14 @@ import { Sidebar } from "../Sidebar";
 const Calendar = () => {
  const [currentMonth, setCurrentMonth] = useState(getMonth())
 
-
  return (
-  <>
-   <div className="h-screen flex flex-columns">
+   <div className="h-screen flex flex-col">
     <CalendarHeader />
     <div className="flex flex-1">
-     <Sidebar />
+     {/* <Sidebar /> */}
      <Month month={currentMonth} />
     </div>
    </div>
-  </>
  )
 };
 
