e140667dca0dfa3da3234add0590b156b814de7f
ahsanali17 <ahsantime1@gmail.com>
2023-02-08 17:09:19 -0500

    changes in app.tsx and css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,15 @@
-import { format } from "date-fns";
-import { useState } from "react";
 import {Calendar} from "./components/Calendar/index";
-import Button from "./components/Button/Button";
 
 const App = () => {
-  const [currentDate, setCurrentDate] = useState(new Date());
-
-  const handleSetToday = () => setCurrentDate(new Date());
-
   return (
-    <div>
-      <div className="flex flex-col items-center gap-4">
-        <p>
+      <>
+        {/* <p>
           <strong>Selected Date: </strong>
           {format(currentDate, "dd LLLL yyyy")}
         </p>
-
-        <Button onClick={handleSetToday}>Today</Button>
-      </div>
-
+        <Button onClick={handleSetToday}>Today</Button> */}
       <Calendar />
-    </div>
+    </>
   );
 };
 
