6795a2af3877f3f15fa43e5d7bd13b55b3df9141
ahsanali17 <ahsantime1@gmail.com>
2023-02-13 03:05:09 -0500

    forgot to remove this provider in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <GoogleOAuthProvider
-        clientId='427300417838-cn2hdh3jkk5pnccfkl5lk4cc0a2uk5b3.apps.googleusercontent.com'
-    >
         <App />
-    </GoogleOAuthProvider>
 )
